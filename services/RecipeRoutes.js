041b775daa44b1d1d4f77a843f96ca2cff561cf4
AntoineCharette <antoineocharette@gmail.com>
2021-09-07 11:05:02 -0400

    adds random recipe path

--- services/RecipeRoutes.js
+++ services/RecipeRoutes.js
@@ -3,10 +3,22 @@ const Recipe = require('../models/Recipe');
 
 const getRecipes = async (req, res) => {
   try {
-    const result = await axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.SPOONACULAR_KEY}&ingredients=${req.query.ingredients}&ranking=1`);
+    const result = await axios.get(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.SPOONACULAR_KEY}&ingredients=${req.query.ingredients}&ranking=1`
+    );
     console.log(result.data);
     res.send(result.data[0]);
-  } catch(err) {
+  } catch (err) {
+    res.status(404).send(err);
+  }
+};
+
+const getRandomRecipes = async (req, res) => {
+  try {
+    const result = await axios.get(`https://api.spoonacular.com/recipes/random?number=5`);
+    console.log(result.data);
+    res.send(result.data);
+  } catch (err) {
     res.status(404).send(err);
   }
 };
@@ -17,25 +29,26 @@ const addRecipe = async (req, res) => {
     const response = await Recipe.create(req.body);
     res.status(201).send(response);
     console.log(response);
-  } catch(err) {
+  } catch (err) {
     res.status(500).send(err);
   }
 };
 
 const deleteRecipe = async (req, res) => {
   console.log('test');
-  try{
+  try {
     const recipeId = req.params.id;
     Recipe.findOneAndDelete(recipeId);
     console.log(recipeId);
     res.send('Deleted Recipe');
-  }catch(err){
+  } catch (err) {
     res.status(500).send(err);
   }
 };
 
 const RecipeRoutes = {
   list: getRecipes,
+  random: getRandomRecipes,
   add: addRecipe,
   delete: deleteRecipe,
 };
